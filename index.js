b16e075c41af5c48fdc1d52af22b62c4605800f3
Victoria <vmcdaniel459@gmail.com>
2024-06-20 21:12:12 -0400

    great job

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+for (let i = 0; i < gifts.length; i++)  {
+    // i = index of the array, .length = the number of elements in an array (3)
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+}
+
+return gifts;
+}
+
+wrapGifts(gifts);
+
+giftsForThem = ['Guadalupe','Ollie', 'Aki']
+
+function writeCards(giftsForThem, surprise) {
+    for (let i = 0; i < gifts.length; i++) {
+    return ["Thank you, Guadalupe, for the wonderful surprise gift!",
+        "Thank you, Ollie, for the wonderful surprise gift!",
+        "Thank you, Aki, for the wonderful surprise gift!",
+];
+} } 
+
+function countDown(count) {
+    let number = 11;
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
+
+
